1bd3824c63ccc93f6df32f6fef79986bedc746de
PayalKarmakar <info.payalkarmakar@gmail.com>
2024-06-30 01:14:32 +0530

    cat modal and sign in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
       { path: "about", element: <Elb.WebsiteAbout /> },
       {
         path: "sign-in",
-        element: <Login />,      
+        element: <Login />,     
         errorElement: <Elb.Error />,
         action: loginAction,
       },
