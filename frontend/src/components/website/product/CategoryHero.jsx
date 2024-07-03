f83ff43272d2d804ae4dbed4fb2c6fa05af6f2c8
unknown <jyotibag9@gmail.com>
2024-07-03 17:53:51 +0530

    pagination

--- frontend/src/components/website/product/CategoryHero.jsx
+++ frontend/src/components/website/product/CategoryHero.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const CategoryHero = ({catSlug}) => {
+    const { listCategories } = useSelector((store) => store.categories);
+    
+    let parentCategory = ''
+    let subcategory = ''
+    let sitem = listCategories.map((item)=>{
+    if(item.slug === catSlug.catname){
+        parentCategory = item.category
+    }
+    
+    if (item?.sub_cat?.find((i)=> i.slug === catSlug.subcat)?.category){
+        subcategory = item?.sub_cat?.find((i)=> i.slug === catSlug.subcat)?.category
+    }
+    })
+   
+
   return (
     <>
       <section
@@ -15,12 +31,12 @@ const CategoryHero = ({catSlug}) => {
                 data-aos-duration="1000"
                 data-aos-easing="linear"
               >
-                <h2 className="section-title-light mb-2">{catSlug.catname}</h2>
+                <h2 className="section-title-light mb-2">{parentCategory}</h2>
                 <nav aria-label="breadcrumb">
                   <ol className="breadcrumb w-breadcrumb">
                     <li className="breadcrumb-item"><a href="#">Home</a></li>
                     <li className="breadcrumb-item active" aria-current="page">
-                    {catSlug.subcat}
+                    {subcategory}
                     </li>
                   </ol>
                 </nav>
