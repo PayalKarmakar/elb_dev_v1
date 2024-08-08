3b9d7ab1aa08912818a966d9f33450a0785ffd6f
Souvik Nag <s.nag26@gmail.com>
2024-08-08 22:47:54 +0530

    post view modal

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -9,7 +9,11 @@ import {
   updateUser,
 } from "../controllers/userController.js";
 import { validateUser } from "../middlewares/userMiddleware.js";
-import { myPostCount, myPosts } from "../controllers/posts/userPosts.js";
+import {
+  myPostCount,
+  myPosts,
+  mySinglePost,
+} from "../controllers/posts/userPosts.js";
 
 router.get(`/all`, allUsers);
 router.get(`/user/:uuid`, getUser);
@@ -19,5 +23,6 @@ router.delete(`/delete/:userId`, deactivateUser);
 router.post(`/activate/:userId`, activateUser);
 router.get(`/my-posts`, myPosts);
 router.get(`/post-count`, myPostCount);
+router.get(`/my-posts/:id`, mySinglePost);
 
 export default router;
